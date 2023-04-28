e773937185de50fd29f097b153a440b195be75cf
YASHU <130467694+nillcoderbot@users.noreply.github.com>
2023-04-28 18:41:43 +0530

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -29,7 +29,7 @@ if Owner_info_msg is None:
 
 **Message Forwards** - {udB.get_key("PMBOT")}
 
-**Ultroid [v{ultroid_version}](https://github.com/TeamUltroid/Ultroid), powered by @TeamUltroid**
+**UserBot [v{ultroid_version}](https://instagram.com/nillcoderbot), powered by @Nillcoderbot**
 """
 
 
@@ -68,7 +68,7 @@ async def own(event):
         mention=event.sender.mention, me=inline_mention(ultroid_bot.me)
     )
     if custom_info:
-        msg += "\n\n• Powered by **@TeamUltroid**"
+        msg += "\n\n• Powered by **@Nillcoderbot**"
     await event.edit(
         msg,
         buttons=[Button.inline("Close", data="closeit")],
